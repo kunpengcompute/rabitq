450768e4e3afb9862bf09d292f5d78fe9bf4a918
lloraine_Liu <liulin162@huawei.com>
2026-02-10 20:21:10 +0800

    feat: add index I/O, search model training pipeline, and SOAR support

---
+++ src/search_model.cpp
@@ -0,0 +1,322 @@
+/*
+   Copyright 2026 Huawei Technologies Co., Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+#define EIGEN_DONT_PARALLELIZE
+#define USE_AVX2
+#include <iostream>
+#include <fstream>
+
+#include <ctime>
+#include <cmath>
+#include "matrix.h"
+#include "utils.h"
+#include "ivf_rabitq.h"
+#include <getopt.h>
+
+#include <omp.h>    
+#include <thread>
+
+using namespace std;
+
+template<uint32_t D, uint32_t B>
+void findTruth(std::vector<int64_t> &approximateGT, float *train_data, float *rand_train_data, int n, int n_leaves, const IVFRN<D, B> &ivf, int k, const char* dataset, float soar_lambda) {
+    std::cerr << "in findTruth n_leaves = " << n_leaves << std::endl;
+    std::cerr << "in findTruth D = " << D << "  BB = " << B << std::endl;
+    for(int i=0;i<n;i++){
+        // std::cerr << "start in i = " << i << std::endl;
+        ResultHeap KNNs = ivf.search(train_data + i * D, rand_train_data + i * B, k, n_leaves, soar_lambda);
+        // std::cerr << "finish i  = " << i << " find truth\n";
+        while(KNNs.empty() == false){
+            approximateGT.push_back(static_cast<int64_t>(KNNs.top().second));
+            KNNs.pop();
+}
+    }
+    
+    char path[512];
+    std::snprintf(path, sizeof(path),
+                  "data/%s/approximateGT.bin", dataset);
+
+
+    std::ofstream of2(path  , std::ios::out | std::ios::binary);
+    of2.write(reinterpret_cast<char *>(approximateGT.data()), approximateGT.size() * sizeof(int64_t));
+    of2.close();
+
+}
+
+template<uint32_t D, uint32_t B>
+void train(const Matrix<float> &Q, const Matrix<float> &RandQ,
+            const IVFRN<D, B> &ivf, int k, std::vector<int64_t> base_labels, int nProbeMax, int qsize, const char* dataset, float soar_lambda){
+    float sys_t, usr_t, usr_t_sum = 0, total_time=0, search_time=0;
+    struct rusage run_start, run_end;
+    const float maxRecall = 0.9999;
+    
+    // const Matrix<float> prob_info;  // Unused variable, commented out
+    std::vector<int64_t> expectedNprobe(qsize);
+
+    // const int num_threads = omp_get_max_threads();
+    // std::vector<int> cpu_ids = getAvailableCPUs();
+
+    // pthread_mutex_t mtx;
+    // pthread_cond_t cond;
+    // bool ready = false;
+    // pthread_mutex_init(&mtx, NULL);
+    // pthread_cond_init(&cond, NULL);
+
+
+    for (int i = 0; i < qsize; ++i){
+        // std::cerr << "star in find nprobe i = " << i << std::endl;
+        int npMin = 1;
+        int npMax = nProbeMax;
+        int curNprobe = (npMin + npMax) / 2;
+        while (npMin <= npMax) {
+            curNprobe = (npMin + npMax) / 2;
+
+            ResultHeap KNNs = ivf.search(Q.data + i * Q.d, RandQ.data + i * RandQ.d, k, curNprobe, soar_lambda);
+            int tmp_correct = 0;
+            while(KNNs.empty() == false){
+                int id = KNNs.top().second;
+                KNNs.pop();
+                for(int j=0;j<k;j++)
+                    if(id == base_labels[i * k + j])tmp_correct ++;
+            }
+            float recall = 1.0f * tmp_correct / (k);
+            if (recall < maxRecall) {
+                npMin = curNprobe + 1;
+            } else if (recall > maxRecall) {
+                npMax = curNprobe - 1;
+            } else {
+                expectedNprobe[i] = curNprobe;
+                break;  // Found exact match, exit loop
+            }
+        }
+        if (expectedNprobe[i] == 0) {
+            expectedNprobe[i] = npMax + 1;
+        }
+    }
+    
+    char path[512];
+    std::snprintf(path, sizeof(path),
+                  "data/%s/expectedNprobe1.bin", dataset);
+
+
+    std::ofstream of2(path, std::ios::out | std::ios::binary);
+    of2.write(reinterpret_cast<char *>(expectedNprobe.data()), expectedNprobe.size()*sizeof(int64_t));
+    of2.close();
+
+}
+
+bool readBinaryFile(const std::string& filename, std::vector<int64_t>& data) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "无法打开文件 " << filename << std::endl;
+        return false;
+    }
+    
+    file.seekg(0, std::ios::end);
+    std::streamsize fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    size_t numElements = fileSize / sizeof(int64_t);
+
+    data.resize(numElements);
+
+    if (file.read(reinterpret_cast<char*>(data.data()), fileSize)) {
+        std::cout << "成功读取 " << numElements << " 个 int64_t 数据" << std::endl;
+    } else {
+        std::cerr << "读取文件时出错" << std::endl;
+        return false;
+    }
+
+    file.close();
+    return true;
+}
+
+int main(int argc, char * argv[]) {
+
+    const struct option longopts[] ={
+        // General Parameter
+        {"help",                        no_argument,       0, 'h'}, 
+
+        // Query Parameter 
+        {"K",                           required_argument, 0, 'k'},
+        {"Nprobe",                      required_argument, 0, 'n'},
+
+        // Indexing Path 
+        {"dataset",                     required_argument, 0, 'd'},
+        {"source",                      required_argument, 0, 's'},
+        {"result_path",                 required_argument, 0, 'r'},
+
+        {"data_path",                   required_argument, 0, 'p'},
+        {"metric_type",                      required_argument, 0, 'm'},
+        {"soar_lambda",                      required_argument, 0, 'a'},
+    };
+
+    int ind;
+    int iarg = 0;
+    opterr = 1;    //getopt error message (off: 0)
+
+    char dataset[256] = "";
+    char source[256] = "";
+    char result_path[256] = "";
+    char data_path[256] = ""; 
+    char metric_type[256] = "";
+    char scan_type[256] = "";
+    
+#if defined(FAST_SCAN)
+    strcpy(scan_type, "fastscan");
+#elif defined(SCAN)
+    char result_file_view[256] = "";
+    strcpy(scan_type, "scan");
+#endif
+    int topk = 10;
+    int n_leaves = 0;
+    float soar_lambda = 0;
+    
+    while(iarg != -1){
+        iarg = getopt_long(argc, argv, "d:r:k:n:s:p:m:a:", longopts, &ind);
+        switch (iarg){
+            case 'k':
+                if(optarg){
+                    topk = atoi(optarg);
+                    std::cout << "TopK set to: " << topk << std::endl;
+                } else {
+                    std::cerr << "Warning: -k option requires an argument, using default value 10" << std::endl;
+                }
+                break;
+            case 'n':
+                if(optarg){
+                    n_leaves = atoi(optarg);
+                    std::cout << "n_leaves set to: " << n_leaves << std::endl;
+                } else {
+                    std::cerr << "Warning: -n option requires an argument, using default value 0" << std::endl;
+                }
+                break;           
+            case 's':
+                if(optarg){
+                    strncpy(source, optarg, sizeof(source) - 1);
+                    source[sizeof(source) - 1] = '\0';
+                }
+                break;
+            case 'r':
+                if(optarg){
+                    strncpy(result_path, optarg, sizeof(result_path) - 1);
+                    result_path[sizeof(result_path) - 1] = '\0';
+                }
+                break;
+            case 'd':
+                if(optarg){
+                    strncpy(dataset, optarg, sizeof(dataset) - 1);
+                    dataset[sizeof(dataset) - 1] = '\0';
+                }
+                break;
+            case 'p':
+                if(optarg){
+                    strncpy(data_path, optarg, sizeof(data_path) - 1);
+                    data_path[sizeof(data_path) - 1] = '\0';
+                }
+                break;
+            case 'm':
+                if(optarg){
+                    strncpy(metric_type, optarg, sizeof(metric_type) - 1);
+                    metric_type[sizeof(metric_type) - 1] = '\0';
+                }
+                break;           
+            case 'a':
+                if(optarg){
+                    soar_lambda = atof(optarg);
+                    std::cout << "soar_lambda set to: " << soar_lambda << std::endl;
+                } else {
+                    std::cerr << "Warning: -a option requires an argument, using default value 0" << std::endl;
+                }
+                break; 
+        }
+    }
+    
+    // ================================================================================================================================
+    // Data Files
+    float *xb_;
+    float *xq_;
+    int64_t *gt_ids_;
+    float *gt_dists_;
+    int32_t nb_, nq_, dim_, gt_closest;
+
+    loadHDF(data_path, nb_, nq_, dim_, gt_closest, xb_, xq_, gt_ids_, gt_dists_, metric_type);
+
+    Matrix<float> Q(xq_, nq_, dim_, false);
+    Matrix<float> X(xb_, nb_, dim_, false);
+    Matrix<int64_t> G(gt_ids_, nq_, gt_closest, false);
+
+    char transformation_path[256] = "";
+    sprintf(transformation_path, "%sP_C%d_B%d.fvecs", source, numC, BB);
+    Matrix<float> P(transformation_path);
+
+    char index_path[256] = "";
+    sprintf(index_path, "%sivfrabitq_%s_%d_B%d.index", source, scan_type, numC, BB);
+    std::cerr << index_path << std::endl;
+#if defined(FAST_SCAN)
+    char result_file_view[256] = "";
+    sprintf(result_file_view, "%s%s_ivfrabitq%d_B%d_fast_scan.log", result_path, dataset, numC, BB);
+#elif defined(SCAN)
+    char result_file_view[256] = "";
+    sprintf(result_file_view, "%s%s_ivfrabitq%d_B%d_scan.log", result_path, dataset, numC, BB);
+#endif
+    std::cerr << "Loading Succeed!" << std::endl;
+    // ================================================================================================================================
+
+    freopen(result_file_view,"a",stdout);
+    
+    IVFRN<DIM, BB> ivf;
+    if (soar_lambda > 0){
+        ivf.use_soar = true;  // Set flag before loading SOAR data
+        ivf.load(index_path);
+    } else {
+        ivf.load(index_path);
+    }
+    Matrix<float> RandX(X.n, BB, X);
+    RandX = mul(RandX, P);
+
+    /// FIND TRUTH
+    int dim = dim_;
+    std::cerr << "dim = " << dim << " random dim = " << RandX.d <<std::endl;
+    int qsize = X.n < 50000 ? X.n : 50000;
+    
+    std::vector<float> train_data(qsize * dim);
+    std::copy(X.data, X.data + qsize * dim, train_data.begin());
+    std::vector<float> rand_train_data(qsize * RandX.d);
+    std::copy(RandX.data, RandX.data + qsize * RandX.d, rand_train_data.begin());
+
+    std::cerr << "--> findTruth dataset = " << dataset << std::endl;
+
+    std::vector<int64_t> approximateGT;
+    findTruth(approximateGT, train_data.data(), rand_train_data.data(), qsize, n_leaves, ivf, topk, dataset, soar_lambda);
+
+    
+    ///// SEARCH EXPECT NPROBE
+    // std::vector<int64_t> approximateGT;
+    // if (readBinaryFile("approximateGT.bin", approximateGT)) {
+    //     // 打印前几个元素以验证
+    //     for (size_t i = 0; i < 10 && i < approximateGT.size(); ++i) {
+    //         std::cerr << approximateGT[i] << " ";
+    //     }
+    //     std::cerr << std::endl;
+    // } else {
+    //     std::cerr << "读取文件失败" << std::endl;
+    // }
+
+    train(X, RandX, ivf, topk, approximateGT, n_leaves, qsize, dataset, soar_lambda);
+    
+    
+    return 0;
+}
--- src/utils.h
+++ src/utils.h
@@ -2,7 +2,8 @@
 #include <chrono>
 #include <queue>
 #include <unordered_set>
-#include <limits>	
+#include <limits>   
+#include <hdf5.h>
 
 #ifndef WIN32
 #include<sys/resource.h>
@@ -10,28 +11,32 @@
 
 typedef std::pair<float, uint32_t> Result;
 typedef std::priority_queue<Result> ResultHeap; 
-	
+
 namespace Detail
 {
-	double constexpr sqrtNewtonRaphson(double x, double curr, double prev)
-	{
-		return curr == prev
-			? curr
-			: sqrtNewtonRaphson(x, 0.5 * (curr + x / curr), curr);
-	}
+    double constexpr sqrtNewtonRaphson(double x, double curr, double prev)
+    {
+        return curr == prev         ? curr          : sqrtNewtonRaphson(x, 0.5 * (curr + x / curr), curr);
 }
+}
+#ifdef __aarch64__
+union Entry {
+    double fvalue;
+};
 
+using PredictFunc = void (*)(union Entry* data, int pred_margin, double* result);
+#endif
 /*
 * Constexpr version of the square root
 * Return value:
-*	- For a finite and non-negative value of "x", returns an approximation for the square root of "x"
+*   - For a finite and non-negative value of "x", returns an approximation for the square root of "x"
 *   - Otherwise, returns NaN
 */
 double constexpr const_sqrt(double x)
 {
-	return x >= 0 && x < std::numeric_limits<double>::infinity()
-		? Detail::sqrtNewtonRaphson(x, x, 0)
-		: std::numeric_limits<double>::quiet_NaN();
+    return x >= 0 && x < std::numeric_limits<double>::infinity()
+        ? Detail::sqrtNewtonRaphson(x, x, 0)
+        : std::numeric_limits<double>::quiet_NaN();
 }
 
 void print_binary(uint64_t v){
@@ -111,13 +116,13 @@ int getRecall(ResultHeap & result, ResultHeap & gt){
 #ifndef WIN32
 void GetCurTime( rusage* curTime)
 {
-	int ret = getrusage( RUSAGE_SELF, curTime);
-	if( ret != 0)
-	{
-		fprintf( stderr, "The running time info couldn't be collected successfully.\n");
-		//FreeData( 2);
-		exit( 0);
-	}
+    int ret = getrusage( RUSAGE_SELF, curTime);
+    if( ret != 0)
+    {
+        fprintf( stderr, "The running time info couldn't be collected successfully.\n");
+        //FreeData( 2);
+        exit( 0);
+    }
 }
 
 /*
@@ -130,14 +135,13 @@ void GetCurTime( rusage* curTime)
 */
 void GetTime( struct rusage* timeStart, struct rusage* timeEnd, float* userTime, float* sysTime)
 {
-	(*userTime) = ((float)(timeEnd->ru_utime.tv_sec - timeStart->ru_utime.tv_sec)) + 
-		((float)(timeEnd->ru_utime.tv_usec - timeStart->ru_utime.tv_usec)) * 1e-6;
-	(*sysTime) = ((float)(timeEnd->ru_stime.tv_sec - timeStart->ru_stime.tv_sec)) +
-		((float)(timeEnd->ru_stime.tv_usec - timeStart->ru_stime.tv_usec)) * 1e-6;
+    (*userTime) = ((float)(timeEnd->ru_utime.tv_sec - timeStart->ru_utime.tv_sec)) + 
+        ((float)(timeEnd->ru_utime.tv_usec - timeStart->ru_utime.tv_usec)) * 1e-6;
+    (*sysTime) = ((float)(timeEnd->ru_stime.tv_sec - timeStart->ru_stime.tv_sec)) +
+        ((float)(timeEnd->ru_stime.tv_usec - timeStart->ru_stime.tv_usec)) * 1e-6;
 }
 
 #endif
-
 #if defined(_WIN32)
 #include <windows.h>
 #include <psapi.h>
@@ -157,7 +161,6 @@ void GetTime( struct rusage* timeStart, struct rusage* timeEnd, float* userTime,
 #elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
 
 #endif
-
 #else
 #error "Cannot define getPeakRSS( ) or getCurrentRSS( ) for an unknown OS."
 #endif
@@ -198,7 +201,6 @@ size_t getPeakRSS() {
 #else
     return (size_t) (rusage.ru_maxrss * 1024L);
 #endif
-
 #else
     /* Unknown OS ----------------------------------------------- */
     return (size_t)0L;          /* Unsupported. */
@@ -243,4 +245,148 @@ size_t getCurrentRSS() {
     /* AIX, BSD, Solaris, and Unknown OS ------------------------ */
     return (size_t)0L;          /* Unsupported. */
 #endif
+}
+
+static const char *HDF5_DATASET_TRAIN = "train";
+static const char *HDF5_DATASET_TEST = "test";
+static const char *HDF5_DATASET_NEIGHBORS = "neighbors";
+static const char *HDF5_DATASET_DISTANCES = "distances";
+ 
+
+void *hdf5_read(const std::string &file_name, const std::string &dataset_name, H5T_class_t dataset_class,
+                int32_t &d_out, int32_t &n_out)
+{
+    hid_t file, dataset, datatype, dataspace, memspace;
+    H5T_class_t t_class;      /* data type class */
+    hsize_t dimsm[3];         /* memory space dimensions */
+    hsize_t dims_out[2];      /* dataset dimensions */
+    hsize_t count[2];         /* size of the hyperslab in the file */
+    hsize_t offset[2];        /* hyperslab offset in the file */
+    hsize_t count_out[3];     /* size of the hyperslab in memory */
+    hsize_t offset_out[3];    /* hyperslab offset in memory */
+    void *data_out = nullptr; /* output buffer */
+ 
+    /* Open the file and the dataset. */
+    file = H5Fopen(file_name.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+    dataset = H5Dopen2(file, dataset_name.c_str(), H5P_DEFAULT);
+ 
+    /* Get datatype and dataspace handles and then query
+     * dataset class, order, size, rank and dimensions. */
+    datatype = H5Dget_type(dataset); /* datatype handle */
+    t_class = H5Tget_class(datatype);
+    // assert(t_class == dataset_class || !"Illegal dataset class type");
+ 
+    dataspace = H5Dget_space(dataset); /* dataspace handle */
+    H5Sget_simple_extent_dims(dataspace, dims_out, nullptr);
+    n_out = dims_out[0];
+    d_out = dims_out[1];
+ 
+    /* Define hyperslab in the dataset. */
+    offset[0] = offset[1] = 0;
+    count[0] = dims_out[0];
+    count[1] = dims_out[1];
+    H5Sselect_hyperslab(dataspace, H5S_SELECT_SET, offset, nullptr, count, nullptr);
+ 
+    /* Define the memory dataspace. */
+    dimsm[0] = dims_out[0];
+    dimsm[1] = dims_out[1];
+    dimsm[2] = 1;
+    memspace = H5Screate_simple(3, dimsm, nullptr);
+ 
+    /* Define memory hyperslab. */
+    offset_out[0] = offset_out[1] = offset_out[2] = 0;
+    count_out[0] = dims_out[0];
+    count_out[1] = dims_out[1];
+    count_out[2] = 1;
+    H5Sselect_hyperslab(memspace, H5S_SELECT_SET, offset_out, nullptr, count_out, nullptr);
+ 
+    /* Read data from hyperslab in the file into the hyperslab in memory and display. */
+    switch (t_class) {
+        case H5T_INTEGER:
+            data_out = new int32_t[dims_out[0] * dims_out[1]];
+            H5Dread(dataset, H5T_NATIVE_INT32, memspace, dataspace, H5P_DEFAULT, data_out);  // read error
+ 
+            break;
+        case H5T_FLOAT:
+            std::cerr << "dims_out[0] = " << dims_out[0] << " dims_out[1] = " << dims_out[1] << std::endl;
+            data_out = new float[dims_out[0] * dims_out[1]];
+            
+            H5Dread(dataset, H5T_NATIVE_FLOAT, memspace, dataspace, H5P_DEFAULT, data_out);
+            break;
+        default:
+            printf("Illegal dataset class type\n");
+            break;
+    }
+ 
+    /* Close/release resources. */
+    H5Tclose(datatype);
+    H5Dclose(dataset);
+    H5Sclose(dataspace);
+    H5Sclose(memspace);
+    H5Fclose(file);
+ 
+    return data_out;
+}
+
+void normalizeVector(float* data, int size) {
+    // Calculate the norm
+    float norm = 0.0;
+    for (int i = 0; i < size; ++i) {
+        norm += data[i] * data[i];
+    }
+    norm = std::sqrt(norm);
+ 
+    // Check if the norm is zero
+    if (norm == 0) {
+        for (int i = 0; i < size; ++i) {
+            data[i] = 1.0f / std::sqrt(size);
+        }
+    } else {
+        // Normalize the vector
+        for (int i = 0; i < size; ++i) {
+            data[i] /= norm;
+        }
+    }
+}
+ 
+
+void loadHDFBase(const std::string &ann_file_name, int32_t &nb, int32_t &dim, float *&data, std::string metricType)
+{
+    data = (float *)hdf5_read(ann_file_name, HDF5_DATASET_TRAIN, H5T_FLOAT, dim, nb);
+    if (metricType == "dot_product") {
+        for (int i = 0; i < nb; i ++)
+            normalizeVector(data + i * dim, dim);
+    }
+}
+
+void loadHDF(const std::string &ann_file_name, int32_t &nb, int32_t &nq, int32_t &dim, int32_t &gt_closest,
+    float *&data, float *&queries, int64_t *&gt_ids, float *&gt_dist, std::string metricType)
+{
+    data = (float *)hdf5_read(ann_file_name, HDF5_DATASET_TRAIN, H5T_FLOAT, dim, nb);
+    if (metricType == "dot_product") {
+        for (int i = 0; i < nb; i ++)
+            normalizeVector(data + i * dim, dim);
+    }
+    queries = (float *)hdf5_read(ann_file_name, HDF5_DATASET_TEST, H5T_FLOAT, dim, nq);
+    if (metricType == "dot_product") {
+        for (int i = 0; i < nq; i ++)
+            normalizeVector(queries + i * dim, dim);
+    }
+    int32_t *gt_ids_short = (int32_t *)hdf5_read(ann_file_name, HDF5_DATASET_NEIGHBORS, H5T_INTEGER, gt_closest, nq);
+    gt_ids = new int64_t[gt_closest * nq];
+    for (int i = 0; i < gt_closest * nq; i++) {
+        gt_ids[i] = gt_ids_short[i];
+    }
+    delete[] gt_ids_short;
+    float *dist_short = (float *)hdf5_read(ann_file_name, HDF5_DATASET_DISTANCES, H5T_FLOAT, gt_closest, nq);
+    gt_dist = new float[gt_closest * nq];
+    for (int i = 0; i < gt_closest * nq; i++) {
+        gt_dist[i] = dist_short[i];
+    }
+    delete[] dist_short;
+}
+
+void* upper_bound_aligned_alloc(size_t align_factor, size_t struct_bytes) {
+    size_t upper_size = (struct_bytes + align_factor - 1) & (-align_factor);
+    return aligned_alloc(align_factor, upper_size);
 }
